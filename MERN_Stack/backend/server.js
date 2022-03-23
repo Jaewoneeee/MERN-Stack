16d1035f05d921ac86574a230bd4e597dabd4b1a
jaewoneeee <chou60400@naver.com>
2022-03-23 18:37:59 +0900

    goal controller routes setup

--- MERN_Stack/backend/server.js
+++ MERN_Stack/backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware')
 const  port = process.env.PORT || 5000; //env에서 가져온 port아니면 5000으로 연결
 
 const app = express();
 
+//middleware ??
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
